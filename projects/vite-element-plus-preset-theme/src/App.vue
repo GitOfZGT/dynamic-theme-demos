3aa9fa02593afec74426ef555a0e36e4aa46b4ea
zougt <zgtusers@outlook.com>
2021-12-06 00:40:45 +0800

    feat(): add vite-element-plus-preset-theme

---
+++ projects/vite-element-plus-preset-theme/src/App.vue
@@ -0,0 +1,83 @@
+<script setup>
+import { ref, watch, computed } from "vue";
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import Forms from "./components/forms.vue";
+import { toggleTheme } from "@zougt/vite-plugin-theme-preprocessor/dist/browser-utils";
+</script>
+
+<template>
+  <div style="padding: 20px">
+    <h1 style="text-align: center">vite + elment-plus 预设主题切换示例</h1>
+    <h3 style="text-align: center">
+      element-plus 的按钮主题色时内联的，这里没法切换颜色，后续研究怎么搞
+    </h3>
+    <div style="text-align: center">
+      <el-button
+        type="primary"
+        plain
+        @click="toggleTheme({ scopeName: 'theme-default' })"
+        >默认</el-button
+      >
+
+      <el-button
+        type="primary"
+        plain
+        @click="toggleTheme({ scopeName: 'theme-red' })"
+        style="margin-left: 20px"
+        >红色</el-button
+      >
+    </div>
+    <h3 class="sub-title">
+      这行文字是非组件库的颜色切换演示，之下是组件库的颜色切换
+    </h3>
+    <el-row>
+      <el-button>Default</el-button>
+      <el-button type="primary">Primary</el-button>
+      <el-button>中文</el-button>
+    </el-row>
+    <Forms />
+    <div class="my-plugins">
+      <h4>webpack版本插件支持</h4>
+      <a href="https://github.com/GitOfZGT/some-loader-utils" target="_blank"
+        >@zougt/some-loader-utils</a
+      >
+      <a
+        href="https://github.com/GitOfZGT/theme-css-extract-webpack-plugin"
+        target="_blank"
+        >@zougt/theme-css-extract-webpack-plugin</a
+      >
+      <h4>vite版本插件支持</h4>
+      <a
+        href="https://github.com/GitOfZGT/vite-plugin-theme-preprocessor"
+        target="_blank"
+        >@zougt/vite-plugin-theme-preprocessor</a
+      >
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "./theme/theme-default.scss";
+.sub-title {
+  margin-top: 20px;
+  text-align: center;
+  // 用一个固定颜色 与  主题色 混合之后的颜色
+  color: mix(#b6b450, $color-primary, 50%);
+}
+
+.my-plugins {
+  padding: 10px 20px;
+  position: fixed;
+  z-index: 9;
+  top: 0px;
+  left: 0px;
+  background-color: #fff;
+  a {
+    display: block;
+  }
+  h4 {
+    margin-top: 10px;
+  }
+}
+</style>
---
+++ projects/vite-element-plus-preset-theme/vite.config.js
@@ -0,0 +1,68 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { themePreprocessorPlugin } from "@zougt/vite-plugin-theme-preprocessor";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base:"/dynamic-theme-demos/vite-element-plus-preset-theme",
+  // css: {
+  //   preprocessorOptions: {
+  //     less: {
+  //       javascriptEnabled: true,
+  //     },
+  //   },
+  // },
+  optimizeDeps: {
+    //【注意】 排除 import { toggleTheme } from "@zougt/vite-plugin-theme-preprocessor/dist/browser-utils"; 在vite的缓存依赖
+    exclude: ["@zougt/vite-plugin-theme-preprocessor/dist/browser-utils"],
+  },
+  plugins: [
+    vue(),
+    Components({
+      // 按需导入element-plus组件
+      resolvers: [ElementPlusResolver({ importStyle: "sass" })],
+    }),
+    themePreprocessorPlugin({
+      scss: {
+        // 是否启用任意主题色模式，这里不启用
+        arbitraryMode: false,
+        // 提供多组变量文件
+        multipleScopeVars: [
+          {
+            // 必需
+            scopeName: "theme-default",
+            // path 和 varsContent 必选一个
+            path: path.resolve("src/theme/theme-default.scss"),
+            // varsContent参数等效于 path文件的内容
+            // varsContent:`@primary-color:${defaultPrimaryColor};`
+          },
+          {
+            scopeName: "theme-red",
+            path: path.resolve("src/theme/theme-red.scss"),
+          },
+        ],
+        // 【注意】 css中不是由主题色变量生成的颜色，也让它抽取到主题css内，可以提高权重
+        includeStyleWithColors: [
+          {
+            color: "#ffffff",
+          },
+        ],
+        // // 默认取 multipleScopeVars[0].scopeName
+        // defaultScopeName: "",
+        // 在生产模式是否抽取独立的主题css文件，extract为true以下属性有效
+        extract: false,
+        // // 独立主题css文件的输出路径，默认取 viteConfig.build.assetsDir 相对于 (viteConfig.build.outDir)
+        // outputDir: "",
+        // // 会选取defaultScopeName对应的主题css文件在html添加link
+        // themeLinkTagId: "theme-link-tag",
+        // // "head"||"head-prepend" || "body" ||"body-prepend"
+        // themeLinkTagInjectTo: "head",
+        // // 是否对抽取的css文件内对应scopeName的权重类名移除
+        // removeCssScopeName: false,
+      },
+    }),
+  ],
+});
